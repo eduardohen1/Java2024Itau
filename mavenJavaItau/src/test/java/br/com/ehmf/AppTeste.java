5481aed945141153cfb7b15f55e7786926f5488c
eduardohen1 <eduardohmferreira@gmail.com>
2024-02-16 09:22:31 -0300

    Aula 16_02_24

---
+++ mavenJavaItau/src/test/java/br/com/ehmf/AppTeste.java
@@ -0,0 +1,13 @@
+package br.com.ehmf;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTeste {
+	
+	@Test
+	public void shouldAnswerWithTrue() {
+		assertTrue(true);
+	}
+}
