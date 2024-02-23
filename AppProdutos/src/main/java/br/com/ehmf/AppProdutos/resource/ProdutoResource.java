0d1fd1f5074c186311a74b62932ee926e20378fc
eduardohen1 <eduardohmferreira@gmail.com>
2024-02-23 08:59:23 -0300

    Aula 23_02_24 Criação de endpoints 02

--- AppProdutos/src/main/java/br/com/ehmf/AppProdutos/resource/ProdutoResource.java
+++ AppProdutos/src/main/java/br/com/ehmf/AppProdutos/resource/ProdutoResource.java
@@ -2,11 +2,16 @@ package br.com.ehmf.AppProdutos.resource;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -41,6 +46,28 @@ public class ProdutoResource {
 		if(newProduto == null)
 			return ResponseEntity.notFound().build();
 		return ResponseEntity.ok(newProduto);
+	}
+	
+	@GetMapping("/{id}") //http://localhost:8080/api/produtos/2
+	public ResponseEntity<Optional<Produto>> getById(@PathVariable Long id){
+		Optional<Produto> produto = produtoService.getById(id);
+		if(produto == null)
+			return ResponseEntity.notFound().build();
+		return ResponseEntity.ok(produto);
+	}
+	
+	@PutMapping
+	public ResponseEntity<Produto> update(@RequestBody Produto produto){
+		Produto upProduto = produtoService.update(produto);
+		if(upProduto == null)
+			return ResponseEntity.notFound().build();
+		return ResponseEntity.ok(upProduto);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<?> delete(@PathVariable Long id){
+		produtoService.delete(id);
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT); //status code 204
 	}
 	
 }
